f0c65f8bebb219e10401efe0c7086fce70783383
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-06-30 23:19:49 +0900

    buttonが閉じるアニメーションの追加

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/MainActivity.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/MainActivity.kt
@@ -5,6 +5,7 @@ import android.animation.PropertyValuesHolder
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
@@ -14,7 +15,7 @@ import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
     lateinit var openButton : ImageButton
-
+    private var isOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +23,10 @@ class MainActivity : AppCompatActivity() {
         val buttons: Array<ImageButton> = arrayOf(findViewById(R.id.add_tag_button),findViewById(R.id.add_memo_button),findViewById(R.id.add_task_button))
         openButton =findViewById(R.id.open_add_menu_button)
         openButton.setOnClickListener {
-            openMenu(buttons)
+            if(isOpen)
+                closeMenu(buttons)
+            else
+                openMenu(buttons)
         }
 
 
@@ -38,19 +42,60 @@ class MainActivity : AppCompatActivity() {
         array[1][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 135 / 180).toFloat())
         array[2][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 180 / 180).toFloat())
         array[2][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 180 / 180).toFloat())
-        visiable(buttons)
+        val alpha = PropertyValuesHolder.ofFloat("alpha",0f,1f)
         for (i in array.indices) {
+            visiable(buttons[i])
             val objectAnimator: ObjectAnimator =
-                ObjectAnimator.ofPropertyValuesHolder(buttons[i], array[i][0], array[i][1])
+                ObjectAnimator.ofPropertyValuesHolder(buttons[i], array[i][0], array[i][1], alpha)
             objectAnimator.setDuration(time)
             objectAnimator.start()
         }
+        isOpen=true
     }
-    
 
-    private fun visiable(buttons:Array<ImageButton>){
-        buttons.forEach {
-            it.visibility = View.VISIBLE
+    private fun closeMenu(buttons:Array<ImageButton>){
+        val radius = 300f
+        val time: Long =300
+        val array = Array(buttons.size,{ arrayOfNulls<PropertyValuesHolder>(2)})
+        array[0][0] = PropertyValuesHolder.ofFloat("translationX",  radius * cos(PI* 90 / 180).toFloat(),0f)
+        array[0][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(PI * 90 / 180).toFloat(),0f)
+        array[1][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(PI * 135 / 180).toFloat(),0f)
+        array[1][1] = PropertyValuesHolder.ofFloat("translationY",  -radius * sin(PI * 135 / 180).toFloat(),0f)
+        array[2][0] = PropertyValuesHolder.ofFloat("translationX",  radius * cos(PI * 180 / 180).toFloat(),0f)
+        array[2][1] = PropertyValuesHolder.ofFloat("translationY",  -radius * sin(PI * 180 / 180).toFloat(),0f)
+        val alpha = PropertyValuesHolder.ofFloat("alpha",1f,0f)
+        for (i in array.indices) {
+            val objectAnimator: ObjectAnimator =
+                ObjectAnimator.ofPropertyValuesHolder(buttons[i], array[i][0], array[i][1],alpha)
+            objectAnimator.setDuration(time)
+            objectAnimator.start()
         }
+        isOpen=false
+    }
+//
+//    fun animateAlphaCloseMenu(menuLayout: ConstraintLayout) {
+//        val time: Long = 300
+//
+//        val objectAnimator = ObjectAnimator.ofFloat(menuLayout, "alpha", 0.5f, 0f)
+//        objectAnimator.setDuration(time)
+//        objectAnimator.start()
+////        gone(menuLayout)
+//        isOpen = false
+//    }
+//    fun animateAlphaOpenMenu(menuLayout: ConstraintLayout) {
+//        val time: Long = 300
+//
+//        val objectAnimator = ObjectAnimator.ofFloat(menuLayout, "alpha", 0f, 1f)
+//        objectAnimator.setDuration(time)
+//        objectAnimator.start()
+////        gone(menuLayout)
+//        isOpen = true
+//    }
+
+    private fun visiable(view:View){
+        view.visibility = View.VISIBLE
+    }
+    private fun gone(view:View){
+        view.visibility=View.GONE
     }
 }
