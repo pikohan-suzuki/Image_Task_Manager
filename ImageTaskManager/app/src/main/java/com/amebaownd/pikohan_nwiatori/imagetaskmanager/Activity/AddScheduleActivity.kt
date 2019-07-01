1a6aaef50be9891c7fa21239958fb88d307d9007
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-01 20:43:28 +0900

    schedule追加画面

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
@@ -1,4 +1,34 @@
 package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Activity
 
-class AddScheduleActivity {
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.AddScheduleTabAdapter
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.TabAdapter
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+import kotlinx.android.synthetic.main.activity_add_schedule.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class AddScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_schedule)
+
+        setTabLayout()
+
+    }
+
+    private fun setTabLayout() {
+        val adapter = AddScheduleTabAdapter(supportFragmentManager, this)
+        val viewPager = findViewById<ViewPager>(R.id.add_schedule_viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.add_schedule_tabLayout)
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+        for(i in 0 until adapter.count){
+            val tab =tabLayout.getTabAt(i)!!
+            tab.customView = adapter.getTabView(tabLayout,i)
+        }
+    }
 }
