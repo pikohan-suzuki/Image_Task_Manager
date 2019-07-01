cadd38c700f4cd5d123b10a51c4e6cde1021ae88
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-01 20:42:56 +0900

    schedule追加画面のTabAdapter

---
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Adapter/AddScheduleTabAdapter.kt
@@ -0,0 +1,48 @@
+package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter
+
+import android.content.Context
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment.AddMemoFragment
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment.AddTaskFragment
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment.MemoListFragent
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment.TaskListFragment
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+
+class AddScheduleTabAdapter(fm: FragmentManager, private val context: Context) : FragmentStatePagerAdapter(fm) {
+
+    private val inflater = LayoutInflater.from(context)
+    private val title = arrayOf<String>("Taskの追加", "Memoの追加")
+    private val src = arrayOf<Int>(R.drawable.ic_local_mall_black_24dp,R.drawable.ic_comment_black_24dp)
+    override fun getItem(position: Int): Fragment? {
+        when(position){
+            0->
+                return AddTaskFragment()
+            1->
+                return AddMemoFragment()
+        }
+        return TaskListFragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return title[position]
+    }
+
+    override fun getCount() = title.size
+
+    fun getTabView(tabLayout: TabLayout, position: Int): View {
+        val view = inflater.inflate(R.layout.tab_item, tabLayout, false)
+        val tabTextView = view.findViewById<TextView>(R.id.tab_item_textView)
+        val tabImageView = view.findViewById<ImageView>(R.id.tab_item_imageView)
+        tabTextView.text = getPageTitle(position)
+        tabTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        tabImageView.setImageResource(src[position])
+        return view
+    }
+}
