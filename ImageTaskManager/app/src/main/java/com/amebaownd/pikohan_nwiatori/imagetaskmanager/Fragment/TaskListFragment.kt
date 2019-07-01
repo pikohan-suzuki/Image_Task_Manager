4930f6329a62e04036ca77aa5408a6c582cfc2e2
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-01 20:41:49 +0900

    タスク一覧用のfragment

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Fragment/TaskListFragment.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Fragment/TaskListFragment.kt
@@ -1,4 +1,16 @@
 package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment
 
-class TaskListFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+
+class TaskListFragment(): Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_task_list,container,false)
+        return view
+    }
 }
