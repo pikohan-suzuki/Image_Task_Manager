b6ec5c97a12cfddee71aa0b57e2deee67bdf56ff
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-03 00:10:54 +0900

    タグ表示用のアダプタ

---
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Adapter/TagRecyclerViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter
+
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Data.Tags
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+
+class TagRecyclerViewAdapter(context: Context, private val tags:List<Tags>?) : RecyclerView.Adapter<TagRecyclerViewAdapter.TagListViewHolder>(){
+    private val inflater = LayoutInflater.from(context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewTyape: Int): TagListViewHolder {
+        val view = inflater.inflate(R.layout.tag_row,parent,false)
+        return TagListViewHolder(view)
+    }
+
+    override fun getItemCount()=if(tags!=null) tags.size else 0
+
+    override fun onBindViewHolder(viewHolder: TagListViewHolder, position: Int) {
+        viewHolder.name.text = Editable.Factory.getInstance().newEditable(tags!![position].name)
+        viewHolder.editImageButton.setOnClickListener{
+            viewHolder.name.background = ColorDrawable(R.drawable.abc_edit_text_material)
+        }
+        viewHolder.deleteEditText.setOnClickListener{
+
+        }
+
+    }
+
+    class TagListViewHolder(view:View) : RecyclerView.ViewHolder(view){
+        val name = view.findViewById<EditText>(R.id.tag_row_editText)
+        val editImageButton = view.findViewById<ImageButton>(R.id.tag_row_edit_imageButton)
+        val deleteEditText=view.findViewById<ImageButton>(R.id.tag_row_delete_imageButton)
+    }
+}
