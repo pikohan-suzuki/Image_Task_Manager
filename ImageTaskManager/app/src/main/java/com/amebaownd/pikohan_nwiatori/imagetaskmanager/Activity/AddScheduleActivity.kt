8da54027b7c248fe4d8729d156151aa8e0484cc5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-06 12:27:12 +0900

    db読み出しの追加

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
@@ -1,20 +1,31 @@
 package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Activity
 
+import android.arch.lifecycle.Observer
+import android.arch.persistence.room.Room
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.AddScheduleTabAdapter
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.MemoRecyclerViewAdapter
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.TabAdapter
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Data.AppDatabase
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Fragment.AddMemoFragment
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
 import kotlinx.android.synthetic.main.activity_add_schedule.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class AddScheduleActivity : AppCompatActivity() {
+class AddScheduleActivity : AppCompatActivity(){
+
+    lateinit var db :AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_schedule)
+
+      db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
 
         setTabLayout()
 
