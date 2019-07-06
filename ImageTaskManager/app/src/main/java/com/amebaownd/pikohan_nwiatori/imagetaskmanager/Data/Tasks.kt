01bedd343cac06a29367ee96c083fad8a8cb1388
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-06 15:31:46 +0900

    varに変更

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Data/Tasks.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Data/Tasks.kt
@@ -4,13 +4,12 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import java.sql.Time
 
-
 @Entity
 data class Tasks constructor(
     @PrimaryKey(autoGenerate = true)
-    val taskId:Long,
-    val title:String,
-    val date:Long,
-    val memo:String,
-    val is_active:Boolean
+    var taskId:Long=0,
+    var title:String="",
+    var date:Long=0,
+    var memo:String="",
+    var isActive:Boolean = true
 )
