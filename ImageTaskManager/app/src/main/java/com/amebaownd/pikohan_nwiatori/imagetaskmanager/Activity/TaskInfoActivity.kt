42fe1dd8f31f0d94ea961e2f83d41b6f8264b47a
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-06 12:28:51 +0900

    tableの追加

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/TaskInfoActivity.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/TaskInfoActivity.kt
@@ -1,4 +1,15 @@
 package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Activity
 
-class TaskInfoActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+
+class TaskInfoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_info)
+
+
+    }
 }
--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Data/MemoImages.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Data/MemoImages.kt
@@ -1,9 +1,16 @@
 package com.amebaownd.pikohan_nwiatori.imagetaskmanager.Data
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.net.Uri
 
-@Entity(primaryKeys = ["memoId", "imageId"])
+@Entity(primaryKeys = ["memoId", "order"],
+    foreignKeys = [ForeignKey(entity = Memos::class
+        ,parentColumns = ["memoId"],
+        childColumns = ["memoId"],
+        onDelete = ForeignKey.CASCADE)])
 data class MemoImages constructor(
-    val memoId:Long,
-    val imageId:Long
+    var memoId:Long=0,
+    var order:Int=0,
+    var uri: String=""
 )
