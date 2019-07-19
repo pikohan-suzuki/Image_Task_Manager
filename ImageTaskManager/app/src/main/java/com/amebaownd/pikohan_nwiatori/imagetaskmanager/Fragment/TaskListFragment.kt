17206a16254dfd604d68dcd798bdbbac28ca815f
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-19 18:42:22 +0900

    test

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Fragment/TaskListFragment.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Fragment/TaskListFragment.kt
@@ -16,6 +16,7 @@ class TaskListFragment(): Fragment(){
     lateinit var db:AppDatabase
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_task_list,container,false)
+        val bbb=1
         return view
     }
 
