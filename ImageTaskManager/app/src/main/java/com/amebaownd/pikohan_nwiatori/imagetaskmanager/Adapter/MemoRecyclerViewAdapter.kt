fc2bb95cc0efae4fa79ffd1196d6d6eb7e18f609
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-19 18:39:53 +0900

    コールバックの実装

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Adapter/MemoRecyclerViewAdapter.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Adapter/MemoRecyclerViewAdapter.kt
@@ -14,9 +14,14 @@ import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Activity.MemoInfoActivity
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Data.MemosAndMemoTags
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
 
-class MemoRecyclerViewAdapter(context: Context, private val memosAndMemoTags:List<MemosAndMemoTags>?) :
+interface GetTagName {
+    fun getTagNameById(tagId: Long, textView: TextView)
+}
+
+class MemoRecyclerViewAdapter(context: Context, private val memosAndMemoTags: List<MemosAndMemoTags>?) :
     RecyclerView.Adapter<MemoRecyclerViewAdapter.MemoListViewHolder>() {
     private val inflater = LayoutInflater.from(context)
+    var lisener: GetTagName? = null
     lateinit var view: View
     override fun onCreateViewHolder(parent: ViewGroup, viewTyape: Int): MemoListViewHolder {
         view = inflater.inflate(R.layout.memo_card, parent, false)
@@ -28,18 +33,16 @@ class MemoRecyclerViewAdapter(context: Context, private val memosAndMemoTags:Lis
     override fun onBindViewHolder(viewHolder: MemoListViewHolder, position: Int) {
         viewHolder.title.text = memosAndMemoTags!![position].memos.title
         viewHolder.created.text = SimpleDateFormat("MM/dd  HH:mm").format(memosAndMemoTags[position].memos.created)
-        viewHolder.card.setOnClickListener{
-            val intent = Intent(view.context,MemoInfoActivity::class.java)
-            intent.putExtra("memoId",memosAndMemoTags[position].memos.memoId)
-            val memoIdArray = LongArray(memosAndMemoTags[position].memoTags.size,{ i-> 0})
-            for(i in memosAndMemoTags[position].memoTags.indices)
-                memoIdArray[i]=memosAndMemoTags[position].memoTags[i].tagId
-            intent.putExtra("tagIdList",memoIdArray)
+        viewHolder.card.setOnClickListener {
+            val intent = Intent(view.context, MemoInfoActivity::class.java)
+            intent.putExtra("memoId", memosAndMemoTags[position].memos.memoId)
             view.context.startActivity(intent)
         }
 
-        for(i in memosAndMemoTags[position].memoTags.indices){
-            val tag = inflater.inflate(R.layout.tag,viewHolder.tagLayout,false)
+        for (i in memosAndMemoTags[position].memoTags.indices) {
+            val tag = inflater.inflate(R.layout.tag, viewHolder.tagLayout, false)
+            if (lisener != null)
+                    lisener!!.getTagNameById(memosAndMemoTags[position].memoTags[i].tagId,tag.findViewById<TextView>(R.id.tag_textView))
             viewHolder.tagLayout.addView(tag)
         }
     }
@@ -49,5 +52,9 @@ class MemoRecyclerViewAdapter(context: Context, private val memosAndMemoTags:Lis
         val title = view.findViewById<TextView>(R.id.memo_card_title_textView)
         val created = view.findViewById<TextView>(R.id.memo_card_created)
         val tagLayout = view.findViewById<LinearLayout>(R.id.memo_card_tag_layout)
+    }
+
+    fun setListener(listener: GetTagName) {
+        this.lisener = listener
     }
 }
