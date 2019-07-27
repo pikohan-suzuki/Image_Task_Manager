83144f2694a68f012f594bc63eafe3c9e9fcc320
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-07-27 21:36:15 +0900

    デフォルトのタグを設定

--- ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
+++ ImageTaskManager/app/src/main/java/com/amebaownd/pikohan_nwiatori/imagetaskmanager/Activity/AddScheduleActivity.kt
@@ -8,29 +8,38 @@ import android.support.v7.app.AppCompatActivity
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Adapter.AddScheduleTabAdapter
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.Data.AppDatabase
 import com.amebaownd.pikohan_nwiatori.imagetaskmanager.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class AddScheduleActivity : AppCompatActivity(){
+class AddScheduleActivity : AppCompatActivity() {
 
-    lateinit var db :AppDatabase
-
+    lateinit var db: AppDatabase
+    lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_schedule)
 
-      db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
+        db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
 
         setTabLayout()
+        val isTask = intent.getIntExtra("is_task", 1)
+        if (isTask == 0) {
+            tabLayout.getTabAt(1)!!.select()
+        }
     }
 
     private fun setTabLayout() {
         val adapter = AddScheduleTabAdapter(supportFragmentManager, this)
         val viewPager = findViewById<ViewPager>(R.id.add_schedule_viewPager)
-        val tabLayout = findViewById<TabLayout>(R.id.add_schedule_tabLayout)
+        tabLayout = findViewById<TabLayout>(R.id.add_schedule_tabLayout)
         viewPager.adapter = adapter
         tabLayout.setupWithViewPager(viewPager)
-        for(i in 0 until adapter.count){
-            val tab =tabLayout.getTabAt(i)!!
-            tab.customView = adapter.getTabView(tabLayout,i)
+        for (i in 0 until adapter.count) {
+            val tab = tabLayout.getTabAt(i)!!
+            tab.customView = adapter.getTabView(tabLayout, i)
         }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
